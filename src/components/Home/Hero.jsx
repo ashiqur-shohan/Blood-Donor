00e275eee62cbc42bcd9cd5a32312895090ba982
ashiqur-shohan <ashiqur.shohan@gmail.com>
2024-10-06 21:16:52 +0600

    hero section register button adjusted for mobile device

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
         alt="Blood donation Hero image"
         className="w-full h-auto rounded-lg shadow-lg"
       />
-      <div className=" absolute bottom-28 lg:bottom-80 left-10 lg:left-28">
+      <div className=" absolute bottom-24 lg:bottom-80 left-10 lg:left-28">
         <button className=" text-sm  lg:font-semibold px-2 lg:px-6 lg:py-3 rounded-sm lg:rounded-lg bg-red-600 hover:bg-red-700 text-white transition duration-300">
           <span className="hidden lg:block">Register as Donor</span>
           <span className="block lg:hidden">Register</span>
