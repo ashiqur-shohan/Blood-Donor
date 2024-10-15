3f5daa82e89f00f639969b99d0f6b261429aa79c
ashiqur-shohan <ashiqur.shohan@gmail.com>
2024-10-16 01:47:02 +0600

    react toastify container added

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,23 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/routes.jsx";
 import "./App.css";
-import { NextUIProvider } from "@nextui-org/react";
+import { ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <NextUIProvider>
-      <RouterProvider router={router} />
-    </NextUIProvider>
+    <RouterProvider router={router} />
+    <ToastContainer
+      position="top-right"
+      autoClose={3000}
+      hideProgressBar={false}
+      newestOnTop
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+      transition:Bounce
+    />
   </StrictMode>
 );
